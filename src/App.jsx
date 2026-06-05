53cf563753d9df5844ab2dbb6cc7716b030f8035
Manisha Mahajan <132148457+mnmah@users.noreply.github.com>
2026-06-06 05:28:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-=======
-// import Home from "./pages/Home";
-import Home from "./components/Home";
->>>>>>> c4629b04edbe714a2f3e5cc355582d504eae4a1c
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
